749de88ae720d755bfa638f0bc989df71a4a8c17
facundo1996 <ffaacu1@gmail.com>
2020-12-14 14:41:55 -0300

    Home and Projects

---
+++ headerJS.js
@@ -0,0 +1,10 @@
+
+
+/* window.addEventListener('scroll', function(){
+    if (document.body.scrollTop === 0){
+        document.getElementById("NavID").style.background = "rgb(234, 243, 237)"
+    }
+    else{
+        document.getElementById("NavID").style.background="rgb(255, 228, 225)"
+    }
+}) */
